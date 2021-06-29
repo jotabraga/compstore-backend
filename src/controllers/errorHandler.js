79a96bad7eec669cff1bab9942b93506f5579931
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-06-29 18:05:10 -0300

    login route

--- src/controllers/errorHandler.js
+++ src/controllers/errorHandler.js
@@ -1,4 +1,5 @@
 export default function errorHandler(error, res) {
+  console.log(error)
   console.log(error?.details[0].type)
   switch (error?.details[0].type) {
     case "any.required":
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
 import register from "../controllers/register.js";
+import login from "../controllers/login.js";
 
 const routes = express.Router();
 
 routes.post("/register", (req, res) => register(req, res));
+
+routes.post("/login", (req, res) => login(req, res));
 
 routes.use((_, res) => res.send("404: Page not found"));
 
