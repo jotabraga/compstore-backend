b470251f3e4d1c95d7446b9b193340c5a8ab0c47
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-02 11:23:49 -0300

    Prettier

--- src/controllers/getCart.js
+++ src/controllers/getCart.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 import { connectionDB } from "../config/database.js";
 import errorHandler from "./errorHandler.js";
@@ -7,9 +7,9 @@ export default async function getCart(req, res) {
   try {
     const token = req.headers["authorization"]?.replace("Bearer ", "");
     const secret = process.env.JWT_SECRET;
-    const {userId} = jwt.verify(token, secret)
+    const { userId } = jwt.verify(token, secret);
     if (!userId) return res.sendStatus(401);
-    
+
     const result = await connectionDB.query(
       `SELECT * FROM cart 
       WHERE "userId" = $1`,
